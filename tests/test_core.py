68e88bca264aa84a384b2f48d7a9497a07bb03e3
Kristian André Jakobsen <k.andrejakobsen@gmail.com>
2023-04-24 01:57:20 +0200

    add tests for three cities

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,17 +7,29 @@ from singer_sdk.testing import get_tap_test_class
 from tap_norwaycitybikeapi.tap import TapNorwayCityBikeAPI
 
 
-SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
+OSLO_CONFIG = {
+    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
+    "client_identifier": "andrejakobsen-meltanotap",
+    "city_name": "oslo",
+}
+BERGEN_CONFIG = {
+    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
+    "client_identifier": "andrejakobsen-meltanotap",
+    "city_name": "bergen",
+}
+TRONDHEIM_CONFIG = {
+    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"),
+    "client_identifier": "andrejakobsen-meltanotap",
+    "city_name": "trondheim",
 }
 
-
 # Run standard built-in tap tests from the SDK:
-TestTapNorwayCityBikeAPI = get_tap_test_class(
-    tap_class=TapNorwayCityBikeAPI,
-    config=SAMPLE_CONFIG
+TestTapOsloCityBikeAPI = get_tap_test_class(
+    tap_class=TapNorwayCityBikeAPI, config=OSLO_CONFIG
 )
-
-
-# TODO: Create additional tests as appropriate for your tap.
+TestTapBergenCityBikeAPI = get_tap_test_class(
+    tap_class=TapNorwayCityBikeAPI, config=BERGEN_CONFIG
+)
+TestTapTrondheimCityBikeAPI = get_tap_test_class(
+    tap_class=TapNorwayCityBikeAPI, config=TRONDHEIM_CONFIG
+)
